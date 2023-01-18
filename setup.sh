81cb039958d6122beeba16f61b3ec6870ab5cd91
Quack <vivaladaman@gmail.com>
2023-01-18 23:03:08 +0700

    add docker

--- setup.sh
+++ setup.sh
@@ -24,6 +24,26 @@ sudo dnf install snapd
 snap refresh
 sudo ln -s /var/lib/snapd/snap /snap
 
+#docker
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+
+sudo dnf -y install dnf-plugins-core
+
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
 #node
 sudo snap install node --classic 
 
